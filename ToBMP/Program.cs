30ae776c42f7d549c9c13e916998fddd92333497
松崎好浩 <y.matsuzaki@ris.co.jp>
2022-10-15 16:45:03 +0900

    更新

--- ToBMP/Program.cs
+++ ToBMP/Program.cs
@@ -8,13 +8,16 @@ namespace ToBMP {
             for (int i = 0; i < args.Length; i++) {
                 var path = args[i];
                 var bmpSrc = new Bitmap(path);
+                if (bmpSrc.PixelFormat == PixelFormat.Format24bppRgb) {
+                    continue;
+                }
                 var bmpDst = new Bitmap(((bmpSrc.Width + 3) >> 2) << 2, bmpSrc.Height, PixelFormat.Format24bppRgb);
                 var g = Graphics.FromImage(bmpDst);
                 g.Clear(Color.White);
                 g.DrawImage(bmpSrc, 0, 0);
                 path = Path.GetDirectoryName(path)
                     + "\\" + Path.GetFileNameWithoutExtension(path)
-                    + ".bmp";
+                    + (Path.GetExtension(path) == ".bmp" ? "_24bit.bmp" : ".bmp");
                 System.Console.WriteLine(path);
                 bmpDst.Save(path, ImageFormat.Bmp);
             }
