6c4e8140791713b8a8bcfdb5e196ab0340189c7f
松崎好浩 <y.matsuzaki@ris.co.jp>
2022-10-15 15:05:43 +0900

    追加

---
+++ ToBMP/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace ToBMP {
+    class Program {
+        static void Main(string[] args) {
+            for (int i = 0; i < args.Length; i++) {
+                var path = args[i];
+                if (".bmp" == Path.GetExtension(path)) {
+                    continue;
+                }
+                var bmpSrc = new Bitmap(path);
+                var bmpDst = new Bitmap(((bmpSrc.Width + 3) >> 2) << 2, bmpSrc.Height);
+                var g = Graphics.FromImage(bmpDst);
+                g.DrawImage(bmpSrc, 0, 0);
+                path = Path.GetDirectoryName(path)
+                    + "\\" + Path.GetFileNameWithoutExtension(path)
+                    + ".bmp";
+                System.Console.WriteLine(path);
+                bmpDst.Save(path, ImageFormat.Bmp);
+                bmpSrc.Dispose();
+                bmpDst.Dispose();
+            }
+        }
+    }
+}
