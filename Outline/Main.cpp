20527008750e239264e03108ff6903f04e06b172
松崎好浩 <y.matsuzaki@ris.co.jp>
2022-10-18 13:50:14 +0900

    更新

--- Outline/Main.cpp
+++ Outline/Main.cpp
@@ -6,9 +6,60 @@
 using namespace std;
 
 #include "../CommonLib/Bitmap.h"
-#include "Outline.h"
+#include "../CommonLib/WorkTable.h"
 
 #pragma comment (lib, "CommonLib.lib")
+
+void
+write_outline(Bitmap* pBmp, int32 weight) {
+    const auto size_max = pBmp->size_max;
+    type_worktable table;
+    table.pCells = reinterpret_cast<type_workcell*>(calloc(size_max, sizeof(type_workcell)));
+    if (NULL == table.pCells) {
+        pBmp->error = -1;
+        return;
+    }
+
+    worktable_create(&table, *pBmp);
+    if (table.error != 0) {
+        pBmp->error = -1;
+        return;
+    }
+
+    worktable_write_outline(table, pBmp);
+
+    free(table.pCells);
+    pBmp->error = 0;
+
+    const auto radius = weight / 2;
+    auto pTempPix = reinterpret_cast<byte*>(malloc(size_max * sizeof(byte)));
+    if (NULL == pTempPix) {
+        pBmp->error = -1;
+        return;
+    }
+    memset(pTempPix, DEFINE_COLOR_OFF, size_max);
+    point pos;
+    for (uint32 i = 0; i < size_max; i++) {
+        if (DEFINE_COLOR_ON != pBmp->pPix[i]) {
+            continue;
+        }
+        pTempPix[i] = DEFINE_COLOR_ON;
+        bitmap_get_pos(*pBmp, &pos, i);
+        for (int32 dy = -radius; dy <= radius; dy++) {
+            for (int32 dx = -radius; dx <= radius; dx++) {
+                auto r = sqrt(dx * dx + dy * dy);
+                if (r <= weight / 2.0) {
+                    auto arownd = bitmap_get_index_ofs(*pBmp, pos, dx, dy);
+                    if (ULONG_MAX != arownd) {
+                        pTempPix[arownd] = DEFINE_COLOR_ON;
+                    }
+                }
+            }
+        }
+    }
+    memcpy_s(pBmp->pPix, size_max, pTempPix, size_max);
+    free(pTempPix);
+}
 
 int main(int argc, char* argv[]) {
     // check parameter
@@ -42,14 +93,12 @@ int main(int argc, char* argv[]) {
             continue;
         }
 
-        outline_exec(pBmp);
+        write_outline(pBmp, thickness);
         if (pBmp->error != 0) {
             cout << "bmp convert error... (" << pBmp->error << ")" << endl;
             delete pBmp;
             continue;
         }
-
-        outline_thickness(pBmp, thickness);
 
         // save
         stringstream ss;
