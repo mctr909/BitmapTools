52e9c74ea60a7912ba65466e7ab78802ac82974a
松崎好浩 <y.matsuzaki@ris.co.jp>
2022-12-20 11:06:00 +0900

    更新

--- アウトライン.bat
+++ アウトライン.bat
@@ -1,13 +1,25 @@
 set folder_path=sample_folder
-set thickness=2
+set thickness=3
 
 set source_file=%folder_path%\*.bmp
 set thickened_file=%folder_path%\*_thickness*.bmp
 set delete_file=%folder_path%\*_thickness%thickness%.bmp
+set rename_src_name=_thickness%thickness%_layer1_outline
+set rename_dst_name=
+
 for %%a in (%source_file%) do (
   outline.exe %thickness% %%a
 )
 for %%a in (%thickened_file%) do (
   outline.exe 1 %%a
 )
-del  %delete_file%
+del %delete_file%
+
+cd %~dp0\%folder_path%
+for %%F in ( * ) do call :sub "%%F"
+exit /b
+:sub
+  set FILE_NAME=%1
+  call set FILE_NAME=%%FILE_NAME:%rename_src_name%=%rename_dst_name%%%
+  ren %1 %FILE_NAME%
+  goto :EOF
