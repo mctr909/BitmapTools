f532a0a1619a43677ada7cc7a9907c831c653766
松崎好浩 <y.matsuzaki@ris.co.jp>
2023-01-21 14:18:04 +0900

    更新

--- メタセコイヤ.bat
+++ メタセコイヤ.bat
@@ -1,9 +1,25 @@
 set folder_path=sample_folder
-set height=20
-set offset_y=0
+set height_1=20
+set height_2=20
+set height_3=20
+set offset_1=0
+set offset_2=20
+set offset_3=40
 
-set source_file=%folder_path%\*_thickness*outline.bmp
-for %%a in (%source_file%) do (
-  OutputMQO.exe %height% %offset_y% %%a
+set layer1_file=%folder_path%\*_layer1.bmp
+set layer2_file=%folder_path%\*_layer2.bmp
+set layer3_file=%folder_path%\*_layer3.bmp
+
+for %%a in (%layer1_file%) do (
+  OutputMQO.exe %height_1% %offset_1% %%a
 )
-del  %source_file%
+for %%a in (%layer2_file%) do (
+  OutputMQO.exe %height_2% %offset_2% %%a
+)
+for %%a in (%layer3_file%) do (
+  OutputMQO.exe %height_3% %offset_3% %%a
+)
+
+del  %layer1_file%
+del  %layer2_file%
+del  %layer3_file%
