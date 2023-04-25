e06f18f8d9bbbb60cc4ce23328ede06f72b8eac3
松崎好浩 <y.matsuzaki@ris.co.jp>
2023-04-25 09:44:23 +0900

    更新

--- Outline/Main.cpp
+++ Outline/Main.cpp
@@ -69,41 +69,43 @@ int main(int argc, char* argv[]) {
     if (argc < 3) {
         cout << "parameter format error..." << endl;
         cout << "[example] Outline.exe <line weight> <bitmap file path>" << endl;
-        return (EXIT_SUCCESS);
+        return (EXIT_FAILURE);
     }
 
     const auto line_weight = atoi(argv[1]);
     string bmp_file = argv[2];
     cout << "BMP FILE : " << bmp_file << endl;
 
+    // get bitmap data
+    auto pBmp = new Bitmap(bmp_file);
+    if (pBmp->error != 0) {
+        cout << "bmp reading error... (" << pBmp->error << ")" << endl;
+        delete pBmp;
+        return (EXIT_FAILURE);
+    } else {
+        pBmp->PrintHeader();
+    }
+
+    // palette chck
+    if (pBmp->info_h.bits != DEFINE_SUPPORT_COLOR_8BIT) {
+        cout << "bmp not support... (only " << DEFINE_SUPPORT_COLOR_8BIT << "bit colors)" << endl;
+        delete pBmp;
+        return (EXIT_FAILURE);
+    }
+
+    // backup input data
+    byte *pInputData = reinterpret_cast<byte*>(malloc(pBmp->size_max));
+    memcpy_s(pInputData, pBmp->size_max, pBmp->pPix, pBmp->size_max);
+
     double layer_lum = 1.0;
     for (int layer = 1; ; layer++) {
-        // get bitmap data
-        auto pBmp = new Bitmap(bmp_file);
-        if (pBmp->error != 0) {
-            cout << "bmp reading error... (" << pBmp->error << ")" << endl;
-            delete pBmp;
-            break;
-        } else {
-            pBmp->PrintHeader();
-        }
-
-        // palette chck
-        if (pBmp->info_h.bits != DEFINE_SUPPORT_COLOR_8BIT) {
-            cout << "bmp not support... (only " << DEFINE_SUPPORT_COLOR_8BIT << "bit colors)" << endl;
-            delete pBmp;
-            break;
-        }
-
         // write outline
         auto has_outline = write_outline(pBmp, line_weight, &layer_lum);
         if (pBmp->error != 0) {
             cout << "bmp convert error... (" << pBmp->error << ")" << endl;
-            delete pBmp;
             break;
         }
         if (!has_outline) {
-            delete pBmp;
             break;
         }
 
@@ -119,15 +121,18 @@ int main(int argc, char* argv[]) {
         pBmp->Save(ss.str());
         if (pBmp->error != 0) {
             cout << "bmp writing error..." << endl;
-            delete pBmp;
             break;
         }
-
-        delete pBmp;
 
         if (layer_lum <= 1.0 / 255.0) {
             break;
         }
+
+        // set input data from backup
+        memcpy_s(pBmp->pPix, pBmp->size_max, pInputData, pBmp->size_max);
+    }
+    if (NULL != pBmp) {
+        delete pBmp;
     }
     return (EXIT_SUCCESS);
 }
