ca0f4556e3cdd1e8829a93f79bb14a3625c4f72f
松嵜義博 <tr-909@hotmail.co.jp>
2023-06-05 07:06:48 +0900

    更新

--- Polygon/script/main.js
+++ Polygon/script/main.js
@@ -15,44 +15,41 @@ initVert(5);
 requestNextAnimationFrame(main);
 
 function main() {
-	gDrawer.clear();
-	loop();
-	requestNextAnimationFrame(main);
+    gDrawer.clear();
+    loop();
+    requestNextAnimationFrame(main);
 }
 function loop() {
-	if (gDrawer.isDrag) {
-		for(let i=0; i<gVertList.length && gDragIndex < 0; i++) {
-			if (distance(gDrawer.cursor, gVertList[i]) <= 10) {
-				gDragIndex = i;
-				break;
-			}
-		}
-	} else {
-		gDragIndex = -1;
-	}
+    if (gDrawer.isDrag) {
+        for(let i=0; i<gVertList.length && gDragIndex < 0; i++) {
+            if (distance(gDrawer.cursor, gVertList[i]) <= 10) {
+                gDragIndex = i;
+                break;
+            }
+        }
+    } else {
+        gDragIndex = -1;
+    }
 
-	if (0 <= gDragIndex) {
-		gDrawer.cursor.copy(gVertList[gDragIndex]);
+    if (0 <= gDragIndex) {
+        gDrawer.cursor.copy(gVertList[gDragIndex]);
         gStep = -1;
-	}
-	for(let i=0; i<gVertList.length; i++) {
-		let p = new vec();
-		gVertList[i].copy(p);
-		p.X += 10000;
-		p.Y -= 10000;
-		gVertInfo[i].distance = p.abs;
-		gVertInfo[i].deleted = false;
-	}
+    }
+    for(let i=0; i<gVertList.length; i++) {
+        let p = new vec();
+        gVertList[i].copy(p);
+        p.X += 10000;
+        p.Y -= 10000;
+        gVertInfo[i].distance = p.abs;
+        gVertInfo[i].deleted = false;
+    }
 
-	gDrawer.drawPolyline(gVertList, [0,0,0]);
-	gDrawer.drawLine(gVertList[gVertList.length-1], gVertList[0], [0,0,0]);
+    gDrawer.drawPolyline(gVertList, [0,0,0]);
+    gDrawer.drawLine(gVertList[gVertList.length-1], gVertList[0], [0,0,0]);
 
-	draw_polygon(1, gStep);
+    draw_polygon(1, gStep);
 }
 function btnReset_onclick(obj) {
-    if (-1 == gStep) {
-        initVert(gVertList.length);
-    }
     gStep = -1;
 }
 function btnPrev_onclick(obj) {
@@ -97,21 +94,21 @@ function initVert(vertCount) {
  * @returns {boolean}
  */
 function inner_triangle(va, vo, vb, p) {
-    let oq_x = va.X - vb.X;
-    let oq_y = va.Y - vb.Y;
-    let op_x = p.X - vb.X;
-    let op_y = p.Y - vb.Y;
-    let normal_abp = oq_x * op_y - oq_y * op_x;
-    oq_x = vo.X - va.X;
-    oq_y = vo.Y - va.Y;
-    op_x = p.X - va.X;
-    op_y = p.Y - va.Y;
-    let normal_oap = oq_x * op_y - oq_y * op_x;
-    oq_x = vb.X - vo.X;
-    oq_y = vb.Y - vo.Y;
-    op_x = p.X - vo.X;
-    op_y = p.Y - vo.Y;
-    let normal_bop = oq_x * op_y - oq_y * op_x;
+    let ba_x = va.X - vb.X;
+    let ba_y = va.Y - vb.Y;
+    let bp_x = p.X - vb.X;
+    let bp_y = p.Y - vb.Y;
+    let normal_abp = ba_x * bp_y - ba_y * bp_x;
+    ba_x = vo.X - va.X;
+    ba_y = vo.Y - va.Y;
+    bp_x = p.X - va.X;
+    bp_y = p.Y - va.Y;
+    let normal_oap = ba_x * bp_y - ba_y * bp_x;
+    ba_x = vb.X - vo.X;
+    ba_y = vb.Y - vo.Y;
+    bp_x = p.X - vo.X;
+    bp_y = p.Y - vo.Y;
+    let normal_bop = ba_x * bp_y - ba_y * bp_x;
     if (normal_abp < 0 && normal_oap < 0 && normal_bop < 0) {
         return true;
     }
@@ -126,50 +123,47 @@ function inner_triangle(va, vo, vb, p) {
  * @param {number} max_step 
  */
 function draw_polygon(order, max_step) {
-	const INDEX_COUNT = gVertList.length;
+    const INDEX_COUNT = gVertList.length;
     const INDEX_NEXT = INDEX_COUNT + order;
-    const INDEX_RIGHT = 1;
-    const INDEX_LEFT = INDEX_COUNT - 1;
-	let reverse_count = 0;
-	let vert_count = 0;
-	let surf_list = new Array();
+    const INDEX_LEFT = 1;
+    const INDEX_RIGHT = INDEX_COUNT - 1;
+    let vert_count = 0;
+    let surf_list = new Array();
     let tri_move = {ia:-1, io:-1, ib:-1};
     let tri_far = {ia:0, io:0, ib:0};
-	let step = 0;
-	do { // 最も遠くにある頂点(vo)の取得ループ
+    step = 0;
+    do { // 最も遠くにある頂点(vo)の取得ループ
         /*** 最も遠くにある頂点(vo)を取得 ***/
         let vo, io = 0;
         let dist_max = 0.0;
-		vert_count = 0;
-		for (let i = 0; i < INDEX_COUNT; i++) {
-			let info = gVertInfo[i];
-			if (info.deleted) {
-				continue;
-			}
-			if (dist_max < info.distance) {
-				dist_max = info.distance;
-				io = i;
-			}
-			vert_count++;
-		}
-		reverse_count = 0;
+        vert_count = 0;
+        for (let i = 0; i < INDEX_COUNT; i++) {
+            let info = gVertInfo[i];
+            if (info.deleted) {
+                continue;
+            }
+            if (dist_max < info.distance) {
+                dist_max = info.distance;
+                io = i;
+            }
+            vert_count++;
+        }
         vo = gVertList[io];
-
         {
             /*** 頂点(vo)の左隣にある頂点(va)を取得 ***/
-            let ia = (io + INDEX_LEFT) % INDEX_COUNT;
+            let ia = (io + INDEX_RIGHT) % INDEX_COUNT;
             for (let i = 0; i < INDEX_COUNT; i++) {
                 if (gVertInfo[ia].deleted) {
-                    ia = (ia + INDEX_LEFT) % INDEX_COUNT;
+                    ia = (ia + INDEX_RIGHT) % INDEX_COUNT;
                 } else {
                     break;
                 }
             }
             /*** 頂点(vo)の右隣にある頂点(vb)を取得 ***/
-            let ib = (io + INDEX_RIGHT) % INDEX_COUNT;
+            let ib = (io + INDEX_LEFT) % INDEX_COUNT;
             for (let i = 0; i < INDEX_COUNT; i++) {
                 if (gVertInfo[ib].deleted) {
-                    ib = (ib + INDEX_RIGHT) % INDEX_COUNT;
+                    ib = (ib + INDEX_LEFT) % INDEX_COUNT;
                 } else {
                     break;
                 }
@@ -182,29 +176,29 @@ function draw_polygon(order, max_step) {
         if (max_step < ++step) {
             break;
         }
+
+        let reverse_count = 0;
         while (true) { // 頂点(vo)の移動ループ
-            /*** 頂点(vo)の左隣にある頂点(va)を取得 ***/
-            let ia = (io + INDEX_LEFT) % INDEX_COUNT;
+            /*** 頂点(vo)の右隣にある頂点(va)を取得 ***/
+            let va, ia = (io + INDEX_RIGHT) % INDEX_COUNT;
             for (let i = 0; i < INDEX_COUNT; i++) {
                 if (gVertInfo[ia].deleted) {
-                    ia = (ia + INDEX_LEFT) % INDEX_COUNT;
+                    ia = (ia + INDEX_RIGHT) % INDEX_COUNT;
                 } else {
                     break;
                 }
             }
-            let va = gVertList[ia];
-            /*** 頂点(vo)の右隣にある頂点(vb)を取得 ***/
-            let ib = (io + INDEX_RIGHT) % INDEX_COUNT;
+            va = gVertList[ia];
+            /*** 頂点(vo)の左隣にある頂点(vb)を取得 ***/
+            let vb, ib = (io + INDEX_LEFT) % INDEX_COUNT;
             for (let i = 0; i < INDEX_COUNT; i++) {
                 if (gVertInfo[ib].deleted) {
-                    ib = (ib + INDEX_RIGHT) % INDEX_COUNT;
+                    ib = (ib + INDEX_LEFT) % INDEX_COUNT;
                 } else {
                     break;
                 }
             }
-            let vb = gVertList[ib];
-            tri_move.ia = ia;
-            tri_move.ib = ib;
+            vb = gVertList[ib];
             /*** 三角形(va vo vb)の表裏を確認 ***/
             let aob_normal = 0;
             let oa_x = va.X - vo.X;
@@ -233,20 +227,20 @@ function draw_polygon(order, max_step) {
                 }
                 vo = gVertList[io];
                 {
-                    /*** 頂点(vo)の左隣にある頂点(va)を取得 ***/
-                    ia = (io + INDEX_LEFT) % INDEX_COUNT;
+                    /*** 頂点(vo)の右隣にある頂点(va)を取得 ***/
+                    ia = (io + INDEX_RIGHT) % INDEX_COUNT;
                     for (let i = 0; i < INDEX_COUNT; i++) {
                         if (gVertInfo[ia].deleted) {
-                            ia = (ia + INDEX_LEFT) % INDEX_COUNT;
+                            ia = (ia + INDEX_RIGHT) % INDEX_COUNT;
                         } else {
                             break;
                         }
                     }
-                    /*** 頂点(vo)の右隣にある頂点(vb)を取得 ***/
-                    ib = (io + INDEX_RIGHT) % INDEX_COUNT;
+                    /*** 頂点(vo)の左隣にある頂点(vb)を取得 ***/
+                    ib = (io + INDEX_LEFT) % INDEX_COUNT;
                     for (let i = 0; i < INDEX_COUNT; i++) {
                         if (gVertInfo[ib].deleted) {
-                            ib = (ib + INDEX_RIGHT) % INDEX_COUNT;
+                            ib = (ib + INDEX_LEFT) % INDEX_COUNT;
                         } else {
                             break;
                         }
@@ -285,20 +279,20 @@ function draw_polygon(order, max_step) {
                 }
                 vo = gVertList[io];
                 {
-                    /*** 頂点(vo)の左隣にある頂点(va)を取得 ***/
-                    ia = (io + INDEX_LEFT) % INDEX_COUNT;
+                    /*** 頂点(vo)の右隣にある頂点(va)を取得 ***/
+                    ia = (io + INDEX_RIGHT) % INDEX_COUNT;
                     for (let i = 0; i < INDEX_COUNT; i++) {
                         if (gVertInfo[ia].deleted) {
-                            ia = (ia + INDEX_LEFT) % INDEX_COUNT;
+                            ia = (ia + INDEX_RIGHT) % INDEX_COUNT;
                         } else {
                             break;
                         }
                     }
-                    /*** 頂点(vo)の右隣にある頂点(vb)を取得 ***/
-                    ib = (io + INDEX_RIGHT) % INDEX_COUNT;
+                    /*** 頂点(vo)の左隣にある頂点(vb)を取得 ***/
+                    ib = (io + INDEX_LEFT) % INDEX_COUNT;
                     for (let i = 0; i < INDEX_COUNT; i++) {
                         if (gVertInfo[ib].deleted) {
-                            ib = (ib + INDEX_RIGHT) % INDEX_COUNT;
+                            ib = (ib + INDEX_LEFT) % INDEX_COUNT;
                         } else {
                             break;
                         }
@@ -331,17 +325,30 @@ function draw_polygon(order, max_step) {
         }
     } while (3 < vert_count); // 最も遠くにある頂点(vo)の取得ループ
 
+    const FONT_SIZE = 18;
+    const DELETED_SIZE = 4;
+    const VERT_SIZE = 11;
+    const VERT_STR_COLOR = [0,231,231];
+    const FAR_COLOR = [231,231,0];
+    const MOVE_COLOR = [0,255,0];
+    const SURF_COLOR = [255,0,0];
+    const FILL_COLOR = [191,191,191];
+    const OFS_A = new vec(4.5, 5.0);
+    const OFS_B = new vec(5.0, 6.0);
+    const OFS_O = new vec(6.0, 6.0);
+
     for(let i=0; i<surf_list.length; i++) {
-        gDrawer.fillPolygon(surf_list[i], new vec(), [211,211,211]);
+        gDrawer.fillPolygon(surf_list[i], new vec(), FILL_COLOR);
         gDrawer.drawPolyline(surf_list[i], [0,0,255], 1);
     }
+
     for(let i=0; i<gVertList.length; i++) {
         let v = gVertList[i];
         if (gVertInfo[i].deleted) {
-            gDrawer.fillCircle(v, 4, [211, 211, 211]);
-            gDrawer.drawCircle(v, 4);
+            gDrawer.fillCircle(v, DELETED_SIZE, [211, 211, 211]);
+            gDrawer.drawCircle(v, DELETED_SIZE);
         } else {
-            gDrawer.fillCircle(v, 9);
+            gDrawer.fillCircle(v, VERT_SIZE);
         }
     }
 
@@ -357,8 +364,10 @@ function draw_polygon(order, max_step) {
         gDrawer.drawArrowC(a, o, [0,0,0], 3);
         gDrawer.drawArrowC(o, b, [0,0,0], 3);
         gDrawer.drawArrowC(b, a, [0,0,0], 3);
-        gDrawer.fillCircle(o, 9, [0,255,0]);
-        gDrawer.drawCircle(o, 9);
+        let surf = [a, o, b, a];
+        gDrawer.fillPolygon(surf, new vec(), SURF_COLOR, 0.2);
+        gDrawer.fillCircle(o, VERT_SIZE, MOVE_COLOR);
+        gDrawer.drawCircle(o, VERT_SIZE);
     }
     let drawFar = !gVertInfo[tri_far.io].deleted;
     if (drawFar) {
@@ -369,28 +378,30 @@ function draw_polygon(order, max_step) {
             gDrawer.drawArrowC(a, o, [0,0,0], 3);
             gDrawer.drawArrowC(o, b, [0,0,0], 3);
             gDrawer.drawArrowC(b, a, [0,0,0], 3);
+            let surf = [a, o, b, a];
+            gDrawer.fillPolygon(surf, new vec(), SURF_COLOR, 0.2);
         }
-        gDrawer.fillCircle(o, 9, [231,231,0]);
-        gDrawer.drawCircle(o, 9);
+        gDrawer.fillCircle(o, VERT_SIZE, FAR_COLOR);
+        gDrawer.drawCircle(o, VERT_SIZE);
         if (drawMove) {
             a = gVertList[tri_move.ia];
             o = gVertList[tri_move.io];
             b = gVertList[tri_move.ib];
-            gDrawer.drawStringXY(o.X - 5, o.Y - 5, "O", 16);
+            gDrawer.drawStringXY(o.X - OFS_O.X, o.Y - OFS_O.Y, "O", FONT_SIZE);
             if (tri_move.ia == tri_far.io) {
-                gDrawer.drawStringXY(a.X - 5, a.Y - 4, "a", 16);
+                gDrawer.drawStringXY(a.X - OFS_A.X, a.Y - OFS_A.Y, "a", FONT_SIZE);
             } else {
-                gDrawer.drawStringXY(a.X - 5, a.Y - 4, "a", 16, [0,255,255]);
+                gDrawer.drawStringXY(a.X - OFS_A.X, a.Y - OFS_A.Y, "a", FONT_SIZE, VERT_STR_COLOR);
             }
             if (tri_move.ib == tri_far.io) {
-                gDrawer.drawStringXY(b.X - 5, b.Y - 5, "b", 16);
+                gDrawer.drawStringXY(b.X - OFS_B.X, b.Y - OFS_B.Y, "b", FONT_SIZE);
             } else {
-                gDrawer.drawStringXY(b.X - 5, b.Y - 5, "b", 16, [0,255,255]);
+                gDrawer.drawStringXY(b.X - OFS_B.X, b.Y - OFS_B.Y, "b", FONT_SIZE, VERT_STR_COLOR);
             }
         } else {
-            gDrawer.drawStringXY(a.X - 5, a.Y - 4, "a", 16, [0,255,255]);
-            gDrawer.drawStringXY(b.X - 5, b.Y - 5, "b", 16, [0,255,255]);
-            gDrawer.drawStringXY(o.X - 5, o.Y - 5, "O", 16);
+            gDrawer.drawStringXY(a.X - OFS_A.X, a.Y - OFS_A.Y, "a", FONT_SIZE, VERT_STR_COLOR);
+            gDrawer.drawStringXY(b.X - OFS_B.X, b.Y - OFS_B.Y, "b", FONT_SIZE, VERT_STR_COLOR);
+            gDrawer.drawStringXY(o.X - OFS_O.X, o.Y - OFS_O.Y, "O", FONT_SIZE);
         }
     }
 }
