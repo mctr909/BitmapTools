d9b5405ee12c237e5577909eaa1564e0601a5ff6
松崎好浩 <y.matsuzaki@ris.co.jp>
2023-09-14 09:32:36 +0900

    更新

--- CommonLib/WorkTable.cpp
+++ CommonLib/WorkTable.cpp
@@ -211,72 +211,33 @@ WorkTable::WriteOutline(Bitmap* pBmp, int32 line_weight) {
         if (!mp_cells[i].filled) {
             continue;
         }
-        bool nofill_b = !get_cell(i, E_DIRECTION::BOTTOM).filled;
-        bool nofill_r = !get_cell(i, E_DIRECTION::RIGHT).filled;
-        bool nofill_l = !get_cell(i, E_DIRECTION::LEFT).filled;
-        bool nofill_t = !get_cell(i, E_DIRECTION::TOP).filled;
-        int nofill_count = nofill_b ? 1 : 0;
-        nofill_count += nofill_r ? 1 : 0;
-        nofill_count += nofill_l ? 1 : 0;
-        nofill_count += nofill_t ? 1 : 0;
-        if (0 == nofill_count) {
-            bool nofill_bl = !get_cell(i, E_DIRECTION::BOTTOM_L).filled;
-            bool nofill_br = !get_cell(i, E_DIRECTION::BOTTOM_R).filled;
-            bool nofill_tl = !get_cell(i, E_DIRECTION::TOP_L).filled;
-            bool nofill_tr = !get_cell(i, E_DIRECTION::TOP_R).filled;
-            nofill_count = nofill_bl ? 1 : 0;
-            nofill_count += nofill_br ? 1 : 0;
-            nofill_count += nofill_tl ? 1 : 0;
-            nofill_count += nofill_tr ? 1 : 0;
-            if (1 == nofill_count) {
-                int dx, dy;
-                if (nofill_bl) {
-                    dx = -2;
-                    dy = -2;
-                }
-                if (nofill_br) {
-                    dx = 2;
-                    dy = -2;
-                }
-                if (nofill_tl) {
-                    dx = -2;
-                    dy = 2;
-                }
-                if (nofill_tr) {
-                    dx = 2;
-                    dy = 2;
-                }
-                auto pos = mp_cells[i].pos;
-                auto idx_x = bitmap_get_index_ofs(*pBmp, pos, dx, 0);
-                auto idx_y = bitmap_get_index_ofs(*pBmp, pos, 0, dy);
-                if (INVALID_INDEX == idx_x) {
-                    idx_x = i;
-                }
-                if (INVALID_INDEX == idx_y) {
-                    idx_y = i;
-                }
-                if (mp_cells[idx_x].filled && mp_cells[idx_y].filled) {
-                    pBmp->mp_pix[i] = m_color_black;
-                }
-            }
+        bool fill_b = get_cell(i, E_DIRECTION::BOTTOM).filled;
+        bool fill_r = get_cell(i, E_DIRECTION::RIGHT).filled;
+        bool fill_l = get_cell(i, E_DIRECTION::LEFT).filled;
+        bool fill_t = get_cell(i, E_DIRECTION::TOP).filled;
+        int fill_count = fill_b ? 1 : 0;
+        fill_count += fill_r ? 1 : 0;
+        fill_count += fill_l ? 1 : 0;
+        fill_count += fill_t ? 1 : 0;
+        if (0 == fill_count) {
             continue;
         }
-        if (3 <= nofill_count) {
+        if (1 == fill_count) {
             E_DIRECTION dir_a;
             E_DIRECTION dir_b;
-            if (!nofill_b) {
+            if (fill_b) {
                 dir_a = E_DIRECTION::BOTTOM_L;
                 dir_b = E_DIRECTION::BOTTOM_R;
             }
-            if (!nofill_r) {
+            if (fill_r) {
                 dir_a = E_DIRECTION::TOP_R;
                 dir_b = E_DIRECTION::BOTTOM_R;
             }
-            if (!nofill_l) {
+            if (fill_l) {
                 dir_a = E_DIRECTION::TOP_L;
                 dir_b = E_DIRECTION::BOTTOM_L;
             }
-            if (!nofill_t) {
+            if (fill_t) {
                 dir_a = E_DIRECTION::TOP_L;
                 dir_b = E_DIRECTION::TOP_R;
             }
@@ -285,6 +246,48 @@ WorkTable::WriteOutline(Bitmap* pBmp, int32 line_weight) {
             if (fill_a ^ fill_b) {
                 continue;
             }
+        }
+        if (4 == fill_count) {
+            bool fill_bl = get_cell(i, E_DIRECTION::BOTTOM_L).filled;
+            bool fill_br = get_cell(i, E_DIRECTION::BOTTOM_R).filled;
+            bool fill_tl = get_cell(i, E_DIRECTION::TOP_L).filled;
+            bool fill_tr = get_cell(i, E_DIRECTION::TOP_R).filled;
+            fill_count = fill_bl ? 1 : 0;
+            fill_count += fill_br ? 1 : 0;
+            fill_count += fill_tl ? 1 : 0;
+            fill_count += fill_tr ? 1 : 0;
+            if (3 == fill_count) {
+                int dx, dy;
+                if (!fill_bl) {
+                    dx = -2;
+                    dy = -2;
+                }
+                if (!fill_br) {
+                    dx = 2;
+                    dy = -2;
+                }
+                if (!fill_tl) {
+                    dx = -2;
+                    dy = 2;
+                }
+                if (!fill_tr) {
+                    dx = 2;
+                    dy = 2;
+                }
+                auto pos = mp_cells[i].pos;
+                auto idx_dx = bitmap_get_index_ofs(*pBmp, pos, dx, 0);
+                auto idx_dy = bitmap_get_index_ofs(*pBmp, pos, 0, dy);
+                if (INVALID_INDEX == idx_dx) {
+                    idx_dx = i;
+                }
+                if (INVALID_INDEX == idx_dy) {
+                    idx_dy = i;
+                }
+                if (mp_cells[idx_dx].filled && mp_cells[idx_dy].filled) {
+                    pBmp->mp_pix[i] = m_color_black;
+                }
+            }
+            continue;
         }
         auto pos = mp_cells[i].pos;
         for (int32 dy = -FILL_RADIUS; dy <= FILL_RADIUS; dy++) {
@@ -296,74 +299,6 @@ WorkTable::WriteOutline(Bitmap* pBmp, int32 line_weight) {
                         pBmp->mp_pix[arownd] = m_color_black;
                     }
                 }
-            }
-        }
-    }
-    for (uint32 i = 0; i < m_pixel_count; i++) {
-        auto idx = i;
-        while (true) {
-            auto pos = mp_cells[idx].pos;
-            auto idx_r = bitmap_get_index_ofs(*pBmp, pos, 1, 0);
-            auto idx_t = bitmap_get_index_ofs(*pBmp, pos, 0, 1);
-            auto idx_l = bitmap_get_index_ofs(*pBmp, pos, -1, 0);
-            auto idx_b = bitmap_get_index_ofs(*pBmp, pos, 0, -1);
-            bool fill_r, fill_t, fill_l, fill_b;
-            if (INVALID_INDEX == idx_r) {
-                fill_r = false;
-            } else {
-                fill_r = pBmp->mp_pix[idx_r] == m_color_black;
-            }
-            if (INVALID_INDEX == idx_t) {
-                fill_t = false;
-            } else {
-                fill_t = pBmp->mp_pix[idx_t] == m_color_black;
-            }
-            if (INVALID_INDEX == idx_l) {
-                fill_l = false;
-            } else {
-                fill_l = pBmp->mp_pix[idx_l] == m_color_black;
-            }
-            if (INVALID_INDEX == idx_b) {
-                fill_b = false;
-            } else {
-                fill_b = pBmp->mp_pix[idx_b] == m_color_black;
-            }
-            int fill_count = fill_r ? 1 : 0;
-            fill_count += fill_t ? 1 : 0;
-            fill_count += fill_l ? 1 : 0;
-            fill_count += fill_b ? 1 : 0;
-            if (fill_count < 2) {
-                auto idx_tl = bitmap_get_index_ofs(*pBmp, pos, -1, 1);
-                auto idx_tr = bitmap_get_index_ofs(*pBmp, pos, 1, 1);
-                auto idx_bl = bitmap_get_index_ofs(*pBmp, pos, -1, -1);
-                auto idx_br = bitmap_get_index_ofs(*pBmp, pos, 1, -1);
-                if (INVALID_INDEX == idx_tl || INVALID_INDEX == idx_tr ||
-                    INVALID_INDEX == idx_bl || INVALID_INDEX == idx_br) {
-                    break;
-                }
-                if (pBmp->mp_pix[idx_tl] == m_color_black || pBmp->mp_pix[idx_tr] == m_color_black ||
-                    pBmp->mp_pix[idx_bl] == m_color_black || pBmp->mp_pix[idx_br] == m_color_black) {
-                    break;
-                }
-                pBmp->mp_pix[idx] = m_color_white;
-                idx = INVALID_INDEX;
-                if (fill_r) {
-                    idx = idx_r;
-                }
-                if (fill_t) {
-                    idx = idx_t;
-                }
-                if (fill_l) {
-                    idx = idx_l;
-                }
-                if (fill_b) {
-                    idx = idx_b;
-                }
-                if (INVALID_INDEX == idx) {
-                    break;
-                }
-            } else {
-                break;
             }
         }
     }
